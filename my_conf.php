d70394276d4ff61c57322b3dfd178c03d90bc08a
ilyanic-hub <ilyanic20@gmail.com>
2026-08-04 01:40:08 +0300

    Update my_conf.php

--- my_conf.php
+++ my_conf.php
@@ -1,7 +1,7 @@
 <?php
 $tokenauthority='ySN3MDSZIz2jGyzlo8DSba8G1TnmFXvx6VnjT4xjJyCHlvrGYt63zdb2GACb';
 $fbpxid='';
-$drop1_uid = 'UA#3919';
+$drop1_uid = '3919';
 
 $oldprice = 1570;
 $skidka = 20;
