c914999266b0a064f4ec3a9f50ccc999068f0227
ilyanic-hub <ilyanic20@gmail.com>
2026-08-04 02:42:28 +0300

    Update success.php

--- success.php
+++ success.php
@@ -4,7 +4,24 @@ require_once 'my_conf.php';
 // require_once 'my_stat_0976767.php'
 ?>
 <html>
+<!-- TikTok Pixel Code Start -->
+<script>
+!function (w, d, t) {
+  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=o||{};n=document.createElement("script")
+  ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
 
+  ttq.load('D9OIDHJC77U97D5Q804G');
+  ttq.page();
+  
+  // Подія конверсії для TikTok
+  ttq.track('PlaceAnOrder', {
+    content_name: 'Надувне крісло з пуфиком',
+    value: 1249,
+    currency: 'UAH'
+  });
+}(window, document, 'ttq');
+</script>
+<!-- TikTok Pixel Code End -->
 <head>
 	<title>Спасибо за заказ!</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
