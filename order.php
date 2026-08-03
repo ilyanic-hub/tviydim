2b75262f1d9d6d3f65367b3bf6e11e80dc0eaf31
ilyanic-hub <ilyanic20@gmail.com>
2026-08-04 02:08:01 +0300

    Update order.php

--- order.php
+++ order.php
@@ -2,24 +2,24 @@
 include_once './my_conf.php';
 
 if (!isset($_POST['name']) || !isset($_POST['phone'])) {
-    die('Помилка: Не отримано дані з форми (name або phone)');
+    header("Location: /");
+    exit();
 }
 
 $name = trim($_POST['name']);
 $rawPhone = trim($_POST['phone']);
 
-// Форматування телефону
+// Форматування телефону під вимоги Drop1
 $cleanPhone = preg_replace('/[^0-9]/', '', $rawPhone);
 if (strlen($cleanPhone) === 10) {
     $cleanPhone = '38' . $cleanPhone;
 }
+
 if (strlen($cleanPhone) === 12) {
     $phone = substr($cleanPhone, 0, 2) . '(' . substr($cleanPhone, 2, 3) . ')' . substr($cleanPhone, 5, 3) . '-' . substr($cleanPhone, 8, 2) . '-' . substr($cleanPhone, 10, 2);
 } else {
     $phone = $rawPhone;
 }
-
-$clean_uid = preg_replace('/[^0-9]/', '', $drop1_uid);
 
 $headers = array(
     "Authorization: Bearer $tokenauthority",
@@ -27,10 +27,11 @@ $headers = array(
     'Content-Type: application/x-www-form-urlencoded'
 );
 
+// Передаємо UID у чистому вигляді (з усіма буквами!)
 $postfields = array(
     'name'  => $name,
     'phone' => $phone,
-    'uid'   => $clean_uid
+    'uid'   => trim($drop1_uid)
 );
 
 $curl = curl_init('https://drop1.top/api/orders');
@@ -44,14 +45,8 @@ $result = curl_exec($curl);
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 
-// ВІДЛАГОДЖЕННЯ: Виводимо все на екран
-echo "<h2>Результат відправки в Drop1:</h2>";
-echo "<p><b>HTTP Code:</b> " . $httpCode . "</p>";
-echo "<p><b>Відповідь сервера:</b> " . htmlspecialchars($result) . "</p>";
-echo "<hr>";
-echo "<h3>Дані, які відправлялися:</h3>";
-echo "<pre>";
-print_r($postfields);
-echo "</pre>";
+// Перенаправлення на сторінку дякуємо
+$fbpxidParam = isset($fbpxid) ? $fbpxid : '';
+header('Location: /success.php?phone=' . urlencode($rawPhone) . '&uid=' . urlencode($drop1_uid) . '&fbpxid=' . urlencode($fbpxidParam));
 exit();
 ?>
