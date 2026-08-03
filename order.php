7d80a64c0b1207508db7a8e443c9a6e6cf6ca766
ilyanic-hub <ilyanic20@gmail.com>
2026-08-04 02:03:52 +0300

    Update order.php

--- order.php
+++ order.php
@@ -45,8 +45,13 @@ $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 
 // ВІДЛАГОДЖЕННЯ: Виводимо все на екран
-// 4. Перенаправлення на сторінку успіху
-$fbpxidParam = isset($fbpxid) ? $fbpxid : '';
-header('Location: /success.php?phone=' . urlencode($rawPhone) . '&uid=' . urlencode($clean_uid) . '&fbpxid=' . urlencode($fbpxidParam));
+echo "<h2>Результат відправки в Drop1:</h2>";
+echo "<p><b>HTTP Code:</b> " . $httpCode . "</p>";
+echo "<p><b>Відповідь сервера:</b> " . htmlspecialchars($result) . "</p>";
+echo "<hr>";
+echo "<h3>Дані, які відправлялися:</h3>";
+echo "<pre>";
+print_r($postfields);
+echo "</pre>";
 exit();
 ?>
