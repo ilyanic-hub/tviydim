ed6341b14272f4a0dcf9ecf089a0a00d210b3939
ilyanic-hub <ilyanic20@gmail.com>
2026-08-05 16:24:33 +0300

    Update inputmask-fix.js

--- js/inputmask-fix.js
+++ js/inputmask-fix.js
@@ -1,8 +1,26 @@
-$('form').on('submit', function(e){
-    let number = $(this).find('input[name=phone]').val(),
-        pre = /_+/;
+$(document).ready(function() {
+    // 1. Настройка и подключение маски к полю телефона
+    $('input[name="phone"]').inputmask("+38(999)999-99-99", {
+        clearIncomplete: false, // Не стирать введенное, если номер введен не до конца
+        showMaskOnHover: false  // Показывать маску только при фокусе
+    });
+
+    // 2. Валидация при отправке формы
+    $('form').on('submit', function(e) {
+        let phoneInput = $(this).find('input[name="phone"]');
+        let phoneNumber = phoneInput.val();
         
-    if (!!number.match(pre)){
-        e.preventDefault();
-    }
-})
+        // Регулярка ищет символ "_" или проверяет, заполнена ли маска полностью
+        let hasUnderscores = /_+/.test(phoneNumber);
+
+        if (hasUnderscores || !phoneInput.inputmask("isComplete")) {
+            e.preventDefault(); // Блокируем отправку
+            
+            // Подсветка ошибки для пользователя
+            phoneInput.css('border', '2px solid red');
+            alert('Будь ласка, введіть номер телефону повністю!');
+        } else {
+            phoneInput.css('border', ''); // Снимаем подсветку, если всё ок
+        }
+    });
+});
