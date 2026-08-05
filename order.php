4ac3dc4c2ae2e5d4de1f46f9461e3596b75c013f
ilyanic-hub <ilyanic20@gmail.com>
2026-08-05 14:33:15 +0300

    Update order.php

--- order.php
+++ order.php
@@ -9,26 +9,20 @@ if (!isset($_POST['name']) || !isset($_POST['phone'])) {
 $name = trim($_POST['name']);
 $rawPhone = trim($_POST['phone']);
 
-// 1. Очищаємо від усіх символів, крім цифр
+// 1. Очищаємо рядок: залишаємо ТІЛЬКИ цифри (видаляємо +, пробіли, дужки)
 $digits = preg_replace('/[^0-9]/', '', $rawPhone);
 
-// 2. Нормалізуємо до 12 цифр (формат 380XXXXXXXXX)
+// 2. Приводимо до стандартного 12-значного формату (380XXXXXXXXX)
 if (strlen($digits) === 10 && strpos($digits, '0') === 0) {
-    // Ввели 096325... -> робимо 38096325...
-    $cleanPhone = '38' . $digits;
+    // Якщо ввели 096325... -> робимо 38096325...
+    $digits = '38' . $digits;
 } elseif (strlen($digits) === 9) {
-    // Ввели 96325... -> робимо 38096325...
-    $cleanPhone = '380' . $digits;
-} elseif (strlen($digits) === 12 && strpos($digits, '380') === 0) {
-    // Вже прийшло 38096325... -> залишаємо як є
-    $cleanPhone = $digits;
-} else {
-    // Якщо прийшло щось нестандартне, беремо останні 12 цифр або чисті цифри
-    $cleanPhone = $digits;
+    // Якщо ввели 96325... -> робимо 38096325...
+    $digits = '380' . $digits;
 }
 
-// 3. Формуємо один єдиний підсумковий номер із ПЛЮСОМ
-$phone = '+' . ltrim($cleanPhone, '+');
+// 3. Формуємо номер з ОДНИМ плюсом спереду для Drop1 (+380XXXXXXXXX)
+$phone = '+' . $digits;
 
 $headers = array(
     "Authorization: Bearer $tokenauthority",
@@ -54,7 +48,7 @@ $result = curl_exec($curl);
 $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
 
-// Перенаправлення на сторінку дякуємо з чистим номером (без + у URL, щоб не було помилок кодування)
+// Перенаправлення на сторінку дякуємо
 $fbpxidParam = isset($fbpxid) ? $fbpxid : '';
 header('Location: /success.php?phone=' . urlencode($phone) . '&uid=' . urlencode($drop1_uid) . '&fbpxid=' . urlencode($fbpxidParam));
 exit();
